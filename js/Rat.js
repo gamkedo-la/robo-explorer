dd2a00ce50bb58de60956579aac194d632c7ddf2
Ashleigh Morris <ash.morris255@gmail.com>
2021-09-17 11:00:16 -0400

    0th draft of rat frame animation, made some notes for tomorrow

--- js/Rat.js
+++ js/Rat.js
@@ -12,6 +12,7 @@ function ratClass() {
   this.height = 50;
   this.frameX = 0;
   this.frameY = 0;
+  this.frame = 0;
 
   this.reset = function () {
 
@@ -46,12 +47,46 @@ function ratClass() {
   }
 
   this.draw = function(){
-    //console.log('rat draw is reached');
-    var frame = 0;
+
+
+    if(this.frame == 0){
+      //console.log('frame flip is reached');
+      this.frame = 1;
+      //console.log(this.frame);
+    }
+    if(this.frame == 1){
+      this.frame = 0;
+    }
+
+    //two things
+    //1, this is effectively doing nothing, this.frame is just reset
+    //2, I think this should be done at the _end_ of this.draw
+    //oh you could just increment frame once at the end of each this.draw call, try that! tomorrow...
+
+    /*
+    //var frame = 0; move this into a object level prop
+    var frameTime = 10;
+    //TODO, flip frame between 0 and 1, based on a counter
+    for(var i = 0; i <= frameTime; i++){
+      if(i == frameTime){
+        console.log('frame in conditional is...' + frame);
+        if(frame == 0){
+          frame = 1;
+          console.log('frame in "one" conditional is...' + frame);
+        }
+        if(frame == 1){
+          frame = 0;
+        }
+      }
+    }
+    */
+    
+    //console.log(this.frame);
     var ratFrameW = 40;
+    //console.log('frame right before rat is draw is... ' + frame);
     canvasContext.drawImage(
       ratPic,
-      frame * ratFrameW, 0, //top left corner of spritesheet frame
+      this.frame * ratFrameW, 0, //top left corner of spritesheet frame
       ratFrameW, ratPic.height, //size of frame
       this.x - ratPic.width / 2, this.y - ratPic.height / 2, //position on screen, centers image relative to self
       ratFrameW, ratPic.height //size of image on screen
