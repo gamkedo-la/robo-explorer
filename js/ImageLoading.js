16a2b72550b64db5d3fda9088595042a40b26733
Rodrigo Lopez <rodprogramdev@gmail.com>
2021-09-29 23:31:21 +0800

    update spike-sprite

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -51,7 +51,7 @@ function loadImages() {
     { trackType: WORLD_DOOR, theFile: "track_door.png" },
     { trackType: WORLD_TUNNEL_UP, theFile: "world_lowerTunnel.png" },
     { trackType: WORLD_SWORD, theFile: "sword.png" },
-    { trackType: WORLD_TRAP, theFile: "spike1.png" },
+    { trackType: WORLD_TRAP, theFile: "spike-sprite.png" },
     { trackType: WORLD_SPEAR, theFile: "spear2.png" },
     { trackType: WORLD_ARROW, theFile: "world_arrow3.png" },
     { trackType: WORLD_RAT, theFile: "rat-walk-cycle.png" }, //visual feedback, did a WORLD_RAT not get scrubbed in worldGrid thru reset
