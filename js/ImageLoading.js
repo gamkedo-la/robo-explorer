801ec631831bebc93bcca7bce2a468cf7466f4e9
Rodrigo Lopez <rodprogramdev@gmail.com>
2021-10-05 23:24:24 +0800

    add walkcycle5.png complete slingshot frames.

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -41,7 +41,7 @@ function loadImages() {
  
   var imageList = [
      //{ varName: heroPic, theFile: "rocketMan3.png"},
-    { varName: heroPic, theFile: "robo-explorer-walkcycle4.png"},
+    { varName: heroPic, theFile: "robo-explorer-walkcycle5.png"},
     // { varName: waterPic, theFile: "sewerWater.png" },
     { varName: ratPic, theFile: "rat-walk-cycle.png"},
     { varName: bombPic, theFile: "bomb-sprite.png"},
