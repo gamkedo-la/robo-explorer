0a05d52d86c9e00f81b729b410f9c23db3674b25
Rodrigo Lopez <rodprogramdev@gmail.com>
2021-10-07 22:51:21 +0800

    add sewerPipe.png

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -83,6 +83,7 @@ function loadImages() {
     { trackType: WORLD_SKELETON2, theFile: "skeleton3.png" },
     { trackType: WORLD_SKELETON_HEADS, theFile: "skeleton-heads.png" },
     { trackType: WORLD_PIPE_TOP7, theFile: "world_pipe_top7.png" },
+    { trackType: WORLD_SEWER_PIPE, theFile: "sewerPipe.png" },
     { trackType: WORLD_LOST_BOY, theFile: "boy-v1.png" },
     // { trackType: WORLD_COCKROACH_EGG, theFile: "cockroachSprite.png"},
 
