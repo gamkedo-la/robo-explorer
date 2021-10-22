61af979db6d47f0cf5b6f83e30c1e3ce111e0db5
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2021-10-23 03:55:14 +0800

    add forest vines

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -76,7 +76,9 @@ function loadImages() {
     { trackType: WORLD_TREELEAVES4, theFile: "treeLeaves4.png" },
     { trackType: WORLD_TREEVINESLEFT, theFile: "treeVinesLeft.png" },
     { trackType: WORLD_TREEVINESRIGHT, theFile: "treeVinesRight.png" },
+    { trackType: WORLD_VINES, theFile: "vines.png" },
     { trackType: WORLD_WALL, theFile: "bricks6.png" },
+    
     { trackType: WORLD_SLINGSHOT, theFile: "tirador.png" },
     { trackType: WORLD_ROCK, theFile: "rock.png" },
     // { trackType: WORLD_LADDER, theFile: "track_ladder.png" },
