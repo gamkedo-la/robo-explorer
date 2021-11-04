6f83bbf2cac46b21551b63d3cc007fa4fcdd93a1
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2021-11-04 20:50:54 +0800

    add laboratoryTree2.png

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -98,7 +98,7 @@ function loadImages() {
     
     { trackType: WORLD_TREEBRANCH, theFile: "treeBranch.png" },
     { trackType: WORLD_DAGGER, theFile: "dagger.png" },
-    { trackType: WORLD_LABORATORYENTRANCE, theFile: "laboratory.png" },
+    { trackType: WORLD_LABORATORYENTRANCE, theFile: "laboratory2.png" },
     { trackType: WORLD_LABORATORYINSIDE, theFile: "laboratoryInside.png" },
     { trackType: WORLD_LABDOOR, theFile: "labDoor.png" },
     { trackType: WORLD_LABPIPE, theFile: "labPipe.png" },
