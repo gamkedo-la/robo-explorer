2a78cc89d514a35b496b3d72ceed0195e332006d
Chris DeLeon <gamkedo@gmail.com>
2021-11-18 19:35:19 -0800

    Start of credits screen hookup

---
+++ js/Credits.js
@@ -0,0 +1,6 @@
+var showCredits = false;
+
+function drawCredits() {
+  colorRect(0, 0, canvas.width, canvas.height, "black");
+  colorText("C key to toggle credits", canvas.width / 2, canvas.height-10, "white");
+}
